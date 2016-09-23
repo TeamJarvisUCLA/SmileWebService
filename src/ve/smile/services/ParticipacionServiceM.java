8017f80fbe7084737ae156954c552f7669f142df
RosireMaiz <rosire08@gmail.com>
2016-09-23 09:29:40 -0430

    servicio de participa

--- src/ve/smile/services/ParticipacionServiceM.java
+++ src/ve/smile/services/ParticipacionServiceM.java
@@ -1,11 +1,50 @@
 package ve.smile.services;
 
-import javax.ws.rs.Path;
+import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import lights.core.services.FachadaService;
+import ve.smile.dao.ParticipacionDAO;
 import ve.smile.dto.Participacion;
 
 @Path("/ParticipacionService")
 public class ParticipacionServiceM extends FachadaService<Participacion> {
+	
+	@GET
+	@Path("/consultaAllParticipacion")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaAllParticipacion() {
+		try {
+
+			List<Participacion> participaciones = new ParticipacionDAO().findAll();
+
+			return buildAnswerSuccess(participaciones, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
+	
+	@GET
+	@Path("/consultaCantidadParticipacion/{cantidad}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaCantidadParticipacion(@PathParam("cantidad") Integer
+			cantidad) {
+		try {
+
+			List<Participacion> participaciones = new ParticipacionDAO().findParticipacionLimt(cantidad);
+
+			return buildAnswerSuccess(participaciones, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
 
 }
