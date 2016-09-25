e610b44e6b3bd0d25ace562c91d3a405bdc1533f
rafa7 <rafa7>
2016-09-24 20:31:26 -0400

    servicio Generador de Respaldo

--- src/ve/smile/seguridad/services/TablaServiceM.java
+++ src/ve/smile/seguridad/services/TablaServiceM.java
@@ -1,7 +1,4 @@
 package ve.smile.seguridad.services;
-
-import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -12,23 +9,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.hibernate.validator.constraints.Email;
-import org.springframework.format.datetime.joda.MillisecondInstantPrinter;
-
 import com.google.gson.Gson;
 
 import lights.core.services.FachadaService;
 import ve.smile.dao.Configuracion;
-import ve.smile.dao.ConfiguracionDAO;
 import ve.smile.dao.MultimediaDAO;
+import ve.smile.dao.RespaldoDAO;
 import ve.smile.dto.Multimedia;
-import ve.smile.dto.RespaldoTabla;
+import ve.smile.dto.Respaldo;
 import ve.smile.enums.ExtensionEnum;
 import ve.smile.enums.TipoMultimediaEnum;
 import ve.smile.seguridad.dao.TablaDAO;
-import ve.smile.seguridad.dto.Operacion;
 import ve.smile.seguridad.dto.Tabla;
-import ve.smile.seguridad.payload.request.PayloadOperacionRequest;
 import ve.smile.seguridad.payload.request.PayloadTablaRequest;
 
 @Path("/TablaService")
@@ -41,78 +33,55 @@ public class TablaServiceM extends FachadaService<Tabla> {
 
 		PayloadTablaRequest request = gson.fromJson(data,
 				PayloadTablaRequest.class);
-		// HashMap<String, Object> parametro2 = request.getParametros();
+		 HashMap<String, Object> parametros = request.getParametros();
 		// request.setObjetos((List<Tabla>) request.getParametro("lista"));
 		// System.out.println(((ArrayList<Tabla>)request.getParametros().get("lista")).size());
 		List<Tabla> listaTabla = request.getObjetos();
+		
 		try {
-			return respaldarTablas(listaTabla);
+			return respaldarTablas(listaTabla,parametros);
 		} catch (Exception e) {
 			return buildAnswerError(e);
 		}
 
 	}
 
-	public String respaldarTablas(List<Tabla> tablas) throws Exception {
-	    Multimedia multimedia = new Multimedia();
-	    multimedia.setDescripcion("descripsion");
-		multimedia.setExtensionEnum(ExtensionEnum.DOC);
-		multimedia.setNombre("Will a la guerra");
-		multimedia.setTipoMultimediaEnum(TipoMultimediaEnum.IMAGEN);
-		multimedia.setUrl("go brother somos los mas valientes");
-		MultimediaDAO multimediaDAO = new MultimediaDAO();
-		 multimedia = multimediaDAO.save(multimedia);
-		 System.out.println("Id de multimedia: " + multimedia.getIdMultimedia());
-		 Runtime runTime;
-	     Process p;
-	    ProcessBuilder pb;
-		TablaDAO tablaDAO = new TablaDAO();
-		Tabla tabla = new Tabla();
-		RespaldoTabla respaldoTabla = new RespaldoTabla();
-		ConfiguracionDAO configuracionDAO = new ConfiguracionDAO();
-		tablaDAO.versionPostgreSQL();
+	public String respaldarTablas(List<Tabla> tablas, HashMap<String, Object> parametros) throws Exception {
+		Runtime runTime;
+		Process p;
+		ProcessBuilder pb;
 		Map<String, String> propiedades1 = new HashMap<String, String>();
 		Configuracion configuracion = new Configuracion();
 		propiedades1 = configuracion.configuracion();
-		System.out.println(propiedades1.get("puerto"));
-		System.out.println(propiedades1.get("ip"));
-		System.out.println(propiedades1.get("user"));
-		System.out.println(propiedades1.get("contrasenna"));
-		System.out.println(propiedades1.get("database"));
 		String tablasRespaldar = "";
-		System.out.println("funciona gson " + tablas.size());
+		String confiBackup = "C:\\Program Files\\PostgreSQL\\9.3\\bin\\pg_dump.exe,-i,-F,c,-b,-v,-f,";
+		String direcRespaldo = "C:\\RespaldoSmile\\"+parametros.get("nombre")+"."+ExtensionEnum.BACKUP;
+		Respaldo respaldo = new Respaldo(new Multimedia(direcRespaldo, (String)parametros.get("nombre"), (String)parametros.get("descripcion"), ExtensionEnum.BACKUP.ordinal(), TipoMultimediaEnum.RESPALDO.ordinal()), new Date().getTime());
+		respaldo.setListTablas(tablas);
+		RespaldoDAO respaldoDAO = new RespaldoDAO();
+		 respaldo = respaldoDAO.save(respaldo);
 		for (int i = 0; i < tablas.size(); i++) {
-				tablasRespaldar +=   ",-t," +tablas.get(i).getNombre()  ;
-			
+			tablasRespaldar += ",-t," + tablas.get(i).getNombre();
+
 		}
-		// para linux
-		//String confiBackup = "/opt/PostgreSQL/9.3/bin/pg_dump,-i,-F,c,-b,-v,-f,";
-		// para Windows
-		String confiBackup = "C:/Program Files/PostgreSQL/9.3/bin/pg_dump.exe,-i,-F,c,-b,-v,-f,";
-		Date date = new Date();
-		// para linux
-		//String direcRespaldo = "/home/willandher/BackupSmile/"+date.toString()+".backup";
-		String direcRespaldo = "C:/RespaldoSmile/"+date.toString()+".backup";
-		
-		///opt/PostgreSQL/9.3/bin/pg_dump,-i,-F,c,-b,-v,-f,/home/backup.backup,Smile,-t,tb_ayuda
-		String codigTemporal = confiBackup+direcRespaldo+","+propiedades1.get("database")+tablasRespaldar;
+		String codigTemporal = confiBackup + direcRespaldo + tablasRespaldar
+				+ "," + propiedades1.get("database");
 		String[] atato = codigTemporal.split(",");
-		pb = new ProcessBuilder(atato);            
+		try {
+			pb = new ProcessBuilder(atato);
+			pb.environment().put("PGHOST", propiedades1.get("ip"));
+			pb.environment().put("PGPORT", propiedades1.get("puerto"));
+			pb.environment().put("PGUSER", propiedades1.get("user"));
+			pb.environment().put("PGPASSWORD", propiedades1.get("contrasenna"));
+			pb.redirectErrorStream(true);
+			p = pb.start();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+		runTime = Runtime.getRuntime();
+		// Se asignan valores a las variables de PostgreSQL		
 
-        runTime = Runtime.getRuntime();
-
-        //Se asignan valores a las variables de PostgreSQL
-       
-        pb.environment().put("PGHOST", propiedades1.get("ip"));
-        pb.environment().put("PGPORT", propiedades1.get("puerto"));
-        pb.environment().put("PGUSER", propiedades1.get("user"));
-        pb.environment().put("PGPASSWORD", propiedades1.get("contrasenna"));
-        pb.redirectErrorStream(true);
-
-        p = pb.start();
-
-		System.out.println(direcRespaldo);
-		System.out.println(tablasRespaldar);
 		return buildAnswerSuccess(SUCCESS_5);
 	};
 
