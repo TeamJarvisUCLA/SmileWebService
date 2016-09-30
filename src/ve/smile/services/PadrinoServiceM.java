0a98c4a05b68ef0ec862b4bbf902f85f9e3fda13
rafa7 <rafa.rodriguezur@gmail.com>
2016-09-29 21:34:31 -0400

    servicio de incluir postulacion de padrino

--- src/ve/smile/services/PadrinoServiceM.java
+++ src/ve/smile/services/PadrinoServiceM.java
@@ -1,11 +1,49 @@
 package ve.smile.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.FrecuenciaAporteDAO;
+import ve.smile.dao.PadrinoDAO;
+import ve.smile.dao.PersonaDAO;
 import ve.smile.dto.Padrino;
+import ve.smile.enums.EstatusPostuladoEnum;
+import ve.smile.payload.request.PayloadPadrinoRequest;
 
 @Path("/PadrinoService")
 public class PadrinoServiceM extends FachadaService<Padrino> {
+	
+	@POST
+	@Path("/incluirPostulacionPadrino")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String incluirPostulacionPadrino(String data) {
+		Gson gson = new Gson();
+		PayloadPadrinoRequest request = gson.fromJson(data,
+				PayloadPadrinoRequest.class);
+		Map<String, Object> mapa = new HashMap<String, Object>();
+		try {
+			PersonaDAO personaDAO = new PersonaDAO();
+			PadrinoDAO padrinoDAO = new PadrinoDAO();
+			FrecuenciaAporteDAO aporteDAO = new FrecuenciaAporteDAO();
+			personaDAO.save(request.getObjeto().getFkPersona());
+			aporteDAO.save(request.getObjeto().getFkFrecuenciaAporte());
+			request.getObjeto().setEstatusPostulado(EstatusPostuladoEnum.POSTULADO.ordinal());
+			Padrino padrino = padrinoDAO.save(request.getObjeto());
+			mapa.put("id", padrino.getIdPadrino());
+			return buildAnswerSuccess(SUCCESS_4, mapa);
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
+
 
 }
