2543528aac5c88d9fe1871bf8b7f9eca7d356c59
jorgeavenda <jorgeavenda.11@gmail.com>
2016-10-01 11:29:15 -0400

    Servicio para cosultar todos los valores

--- src/ve/smile/services/ValoresServiceM.java
+++ src/ve/smile/services/ValoresServiceM.java
@@ -1,11 +1,32 @@
 package ve.smile.services;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.ValoresDAO;
 import ve.smile.dto.Valores;
 
 @Path("/ValoresService")
 public class ValoresServiceM extends FachadaService<Valores> {
 
+	@GET
+	@Path("/consultarAllValores")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaAllValores() {
+		try {
+
+			List<Valores> valores = new ValoresDAO().findAll();
+
+			return buildAnswerSuccess(valores, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
 }
