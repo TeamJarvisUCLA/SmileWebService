79a36bc277ec18aefbf65d5deb415d09770d01b6
eumir.ollarvez <eumirollarvez@gmail.com>
2016-10-04 22:58:44 -0400

    Cambios para buscar fortalezas por trabajador.

--- src/ve/smile/services/FortalezaServiceM.java
+++ src/ve/smile/services/FortalezaServiceM.java
@@ -1,11 +1,52 @@
 package ve.smile.services;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.FortalezaDAO;
 import ve.smile.dto.Fortaleza;
 
 @Path("/FortalezaService")
 public class FortalezaServiceM extends FachadaService<Fortaleza> {
+
+	@GET
+	@Path("/consultarPorTrabajador/{idSesion}/{accessToken}/{idTrabajador}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String pathConsultarPorTrabajador(
+			@PathParam("idSesion") Integer idSesion,
+			@PathParam("accessToken") String accessToken,
+			@PathParam("idTrabajador") Integer idTrabajador) {
+		try {
+			if (validarSesion(idSesion, accessToken)) {
+				return buildAnswerSuccess(
+						new FortalezaDAO()
+								.findByTrabajador(idTrabajador),
+						SUCCESS_2);
+			}
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+		return buildAnswerError(new Exception(ERROR_UNKNOWN));
+	}
+
+	@GET
+	@Path("/consultarPorTrabajadorSinSession/{idTrabajador}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String pathConsultarPorTrabajadorSinSession(
+			@PathParam("idTrabajador") Integer idTrabajador) {
+		try {
+			return buildAnswerSuccess(
+					new FortalezaDAO()
+							.findByTrabajador(idTrabajador),
+					SUCCESS_2);
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+	}
 
 }
