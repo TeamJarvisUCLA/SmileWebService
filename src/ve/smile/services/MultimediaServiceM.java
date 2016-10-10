2b167835b2f9c465e8b878bded355b51f57ff832
jorgeavenda <jorgeavenda.11@gmail.com>
2016-10-10 15:11:44 -0400

    servicios para album y galeria

--- src/ve/smile/services/MultimediaServiceM.java
+++ src/ve/smile/services/MultimediaServiceM.java
@@ -1,11 +1,34 @@
 package ve.smile.services;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.MultimediaDAO;
 import ve.smile.dto.Multimedia;
 
 @Path("/MultimediaService")
 public class MultimediaServiceM extends FachadaService<Multimedia> {
 
+	@GET
+	@Path("/consultarMultimediaTipo/{cantidad}/{tipo}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultarMultimediaTipo(@PathParam("cantidad") Integer cantidad,
+			@PathParam("tipo") Integer tipo) {
+		try {
+
+			List<Multimedia> albumes = new MultimediaDAO().findMultimediaTipo(cantidad, tipo);
+
+			return buildAnswerSuccess(albumes, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
 }
--- src/ve/smile/services/TsPlanServiceM.java
+++ src/ve/smile/services/TsPlanServiceM.java
@@ -1,11 +1,33 @@
 package ve.smile.services;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.TsPlanDAO;
 import ve.smile.dto.TsPlan;
 
 @Path("/TsPlanService")
 public class TsPlanServiceM extends FachadaService<TsPlan> {
 
+	@GET
+	@Path("/consultaTSPlanificadoAlbum/{album}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaTSPlanificadoAlbum(@PathParam("album") Integer album) {
+		try {
+
+			List<TsPlan> ts = new TsPlanDAO().findTSPlanificadoAlbum(album);
+
+			return buildAnswerSuccess(ts, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
 }
