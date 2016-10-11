47c5cda03aa58a653adc68123cfba546e35d20de
willandherg <willandherg>
2016-10-11 06:56:00 -0400

    Reporte Voluntario

--- src/ve/smile/services/VoluntarioServiceM.java
+++ src/ve/smile/services/VoluntarioServiceM.java
@@ -1,11 +1,37 @@
 package ve.smile.services;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.CarteleraDAO;
+import ve.smile.dao.VoluntarioDAO;
+import ve.smile.dto.Cartelera;
 import ve.smile.dto.Voluntario;
 
 @Path("/VoluntarioService")
 public class VoluntarioServiceM extends FachadaService<Voluntario> {
 
+	@GET
+	@Path("/consultaVoluntariosParametrizado/{sql}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaVoluntariosParametrizado(@PathParam("sql") String
+			sql) {
+		try {
+
+			List<Voluntario> voluntarios = new VoluntarioDAO().consultaVoluntariosParametrizado(sql);
+
+			return buildAnswerSuccess(voluntarios, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
+	
 }
