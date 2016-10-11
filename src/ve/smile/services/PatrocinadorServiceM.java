96b9e411fa7dcf3690ea5f964f16abc4984ef842
jorgeavenda <jorgeavenda.11@gmail.com>
2016-10-11 13:29:12 -0400

    service patrocinador

--- src/ve/smile/services/PatrocinadorServiceM.java
+++ src/ve/smile/services/PatrocinadorServiceM.java
@@ -1,11 +1,32 @@
 package ve.smile.services;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.PatrocinadorDAO;
 import ve.smile.dto.Patrocinador;
 
 @Path("/PatrocinadorService")
 public class PatrocinadorServiceM extends FachadaService<Patrocinador> {
 
+	@GET
+	@Path("/consultarAllPatrocinador")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultarAllPatrocinador() {
+		try {
+
+			List<Patrocinador> patrocinadores = new PatrocinadorDAO().findAll();
+
+				return buildAnswerSuccess(patrocinadores, SUCCESS_2);
+
+			} catch (Exception e) {
+				return buildAnswerError(e);
+			}
+
+	}
 }
