afffd41469183764f3497f2513db3022e51419ee
RosireMaiz <rosire08@gmail.com>
2016-10-18 10:03:55 -0400

    Modificaciones en servicio para trabajadores en reportes

--- src/ve/smile/services/TrabajadorServiceM.java
+++ src/ve/smile/services/TrabajadorServiceM.java
@@ -1,11 +1,34 @@
 package ve.smile.services;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.TrabajadorDAO;
 import ve.smile.dto.Trabajador;
 
 @Path("/TrabajadorService")
 public class TrabajadorServiceM extends FachadaService<Trabajador> {
 
+	@GET
+	@Path("/consultaTrabajadoresParametrizado/{sql}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaTrabajadoresParametrizado(@PathParam("sql") String sql) {
+		try {
+
+			List<Trabajador> trabajadors = new TrabajadorDAO()
+					.consultaTrabajadoresParametrizado(sql);
+
+			return buildAnswerSuccess(trabajadors, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
 }
