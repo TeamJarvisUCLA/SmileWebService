d7fe216589d5b7b508da330870b1d480148ccd0f
RosireMaiz <rosire08@gmail.com>
2016-10-23 16:04:25 -0400

    Modificaciones para servicio de reporte de ayudas

--- src/ve/smile/services/SolicitudAyudaServiceM.java
+++ src/ve/smile/services/SolicitudAyudaServiceM.java
@@ -1,11 +1,35 @@
 package ve.smile.services;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import lights.core.services.FachadaService;
+import ve.smile.dao.SolicitudAyudaDAO;
 import ve.smile.dto.SolicitudAyuda;
 
 @Path("/SolicitudAyudaService")
 public class SolicitudAyudaServiceM extends FachadaService<SolicitudAyuda> {
+
+	@GET
+	@Path("/consultaSolicitudesParametrizado/{sql}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaSolicitudesParametrizado(@PathParam("sql") String sql) {
+		try {
+
+			List<SolicitudAyuda> solicitudAyudas = new SolicitudAyudaDAO()
+					.consultaSolicitudesParametrizado(sql);
+
+			return buildAnswerSuccess(solicitudAyudas, SUCCESS_2);
+
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
 
 }
