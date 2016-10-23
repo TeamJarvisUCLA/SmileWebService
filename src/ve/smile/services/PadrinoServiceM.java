819cfc1b111029f7ff59025ccc7d0ca9cdb79ce0
RosireMaiz <rosire08@gmail.com>
2016-10-23 03:47:05 -0400

    Modificaciones para servicio de reportes

--- src/ve/smile/services/PadrinoServiceM.java
+++ src/ve/smile/services/PadrinoServiceM.java
@@ -1,26 +1,41 @@
 package ve.smile.services;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.google.gson.Gson;
-
 import lights.core.services.FachadaService;
-import ve.smile.dao.FrecuenciaAporteDAO;
 import ve.smile.dao.PadrinoDAO;
 import ve.smile.dao.PersonaDAO;
 import ve.smile.dto.Padrino;
 import ve.smile.enums.EstatusPadrinoEnum;
 import ve.smile.payload.request.PayloadPadrinoRequest;
 
+import com.google.gson.Gson;
+
 @Path("/PadrinoService")
 public class PadrinoServiceM extends FachadaService<Padrino> {
-	
+	@GET
+	@Path("/consultaPadrinosParametrizado/{sql}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public String consultaPadrinosParametrizado(@PathParam("sql") String sql) {
+		try {
+			List<Padrino> padrinos = new PadrinoDAO()
+					.consultaPadrinosParametrizado(sql);
+			return buildAnswerSuccess(padrinos, SUCCESS_2);
+		} catch (Exception e) {
+			return buildAnswerError(e);
+		}
+
+	}
+
 	@POST
 	@Path("/incluirPostulacionPadrino")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -31,9 +46,10 @@ public class PadrinoServiceM extends FachadaService<Padrino> {
 		Map<String, Object> mapa = new HashMap<String, Object>();
 		try {
 			PersonaDAO personaDAO = new PersonaDAO();
-			PadrinoDAO padrinoDAO = new PadrinoDAO();		
+			PadrinoDAO padrinoDAO = new PadrinoDAO();
 			personaDAO.save(request.getObjeto().getFkPersona());
-			request.getObjeto().setEstatusPadrino(EstatusPadrinoEnum.POSTULADO.ordinal());
+			request.getObjeto().setEstatusPadrino(
+					EstatusPadrinoEnum.POSTULADO.ordinal());
 			Padrino padrino = padrinoDAO.save(request.getObjeto());
 			mapa.put("id", padrino.getIdPadrino());
 			return buildAnswerSuccess(SUCCESS_4, mapa);
@@ -42,6 +58,5 @@ public class PadrinoServiceM extends FachadaService<Padrino> {
 		}
 
 	}
-
 
 }
